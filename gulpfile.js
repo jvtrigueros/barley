b9c71304aeeb404ada8eb07d08c1671f15ff2d1e
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-02-26 21:37:11 -0700

    Create inital gulpfile and include dependencies.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+  , less = require('gulp-less')
+  , uglify = require('gulp-uglify')
+  , cssmin = require('gulp-cssmin')
+  , gutil = require('gulp-util')
+
+gulp.task('default', function () {
+  gutil.log('Doing nothing yet.')
+})
