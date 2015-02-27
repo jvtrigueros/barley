9278d69b12ab711dc2c751d45a100eab81cf57fa
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-02-26 22:03:24 -0700

    Complete less task.
    - Compiling less
    - Minifying CSS
    - Renaming and placing in correct directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
+var path = require('path')
+
 var gulp = require('gulp')
-  , less = require('gulp-less')
-  , uglify = require('gulp-uglify')
   , cssmin = require('gulp-cssmin')
   , gutil = require('gulp-util')
+  , less = require('gulp-less')
+  , rename = require('gulp-rename')
+  , uglify = require('gulp-uglify')
 
 gulp.task('default', function () {
   gutil.log('Doing nothing yet.')
+})
+
+gulp.task('less', function () {
+  gutil.log('Compiling less files.')
+  return gulp.src('less/app.less')
+    .pipe(less({
+      paths: [path.join(__dirname, 'less')]
+    }))
+    .pipe(cssmin())
+    .pipe(rename({basename: 'style'}))
+    .pipe(gulp.dest('assets/css'))
 })
